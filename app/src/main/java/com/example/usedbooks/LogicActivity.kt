e031796615e7585a24a3874097808cad9b43d4d4
Javaninja-Lab <luigi.kart30@gmail.com>
2022-04-13 09:40:56 +0200

    Creazione activity_logic.xml

---
+++ app/src/main/java/com/example/usedbooks/LogicActivity.kt
@@ -0,0 +1,11 @@
+package com.example.usedbooks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogicActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logic)
+    }
+}
