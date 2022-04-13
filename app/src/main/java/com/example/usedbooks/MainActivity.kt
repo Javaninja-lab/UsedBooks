d0c11a2bd6546dbdfee890a8f3f604ae74e3c9dd
Zeref-zt <lorenzo.ficazzola@outlook.it>
2022-04-13 16:15:40 +0200

    sistemazione grafica login page

--- app/src/main/java/com/example/usedbooks/MainActivity.kt
+++ app/src/main/java/com/example/usedbooks/MainActivity.kt
@@ -2,15 +2,14 @@ package com.example.usedbooks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_logic.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logic)
-        //test
-        val x=0
-        val y=0
-        //test2
-        val z=0
+
+
+
     }
 }
