72d7e35609e107355f8b434f01b1307d80da7201
Zeref-zt <lorenzo.ficazzola@outlook.it>
2022-04-13 11:50:20 +0200

    login, problema button sign in

--- app/src/main/java/com/example/usedbooks/MainActivity.kt
+++ app/src/main/java/com/example/usedbooks/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_logic)
         //test
         val x=0
         val y=0
