f8839895a97f603b42c4d2a934bb5cec711cdb5d
Javaninja-Lab <luigi.kart30@gmail.com>
2022-04-14 11:25:32 +0200

    Creazione activity_main.xml e piccole migliorie grafiche activity_login.xml e activity_register.xml

--- app/src/main/java/com/example/usedbooks/LoginActivity.kt
+++ app/src/main/java/com/example/usedbooks/LoginActivity.kt
@@ -15,5 +15,10 @@ class LoginActivity : AppCompatActivity() {
             startActivity(i)
             overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left)
         }
+        BtnLoginActivityLogin.setOnClickListener {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            //overridePendingTransition(androidx.appcompat.R.anim.abc_slide_out_bottom, androidx.appcompat.R.anim.abc_slide_in_top)
+        }
     }
 }
