a0f7157f46afe2f2c6e8cf8e8283486d0548c6af
Javaninja-Lab <luigi.kart30@gmail.com>
2022-04-19 15:23:48 +0200

    Inizio implementazione connessione database tramite classe Database.kt

--- app/src/main/java/com/example/usedbooks/MainActivity.kt
+++ app/src/main/java/com/example/usedbooks/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.usedbooks
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -16,26 +17,30 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction().add(R.id.fragmentContainerViewMainActivity, HomeFragment()).commit()
 
         bottomNavigationView.setOnItemSelectedListener {
-            var itemSelected : Fragment = HomeFragment()
-            when(it.itemId) {
-                R.id.home -> {
-                    itemSelected = HomeFragment()
+                var itemSelected: Fragment = HomeFragment()
+                when (it.itemId) {
+                    R.id.home -> {
+                        itemSelected = HomeFragment()
+                    }
+                    R.id.search -> {
+                        itemSelected = SearchFragment()
+                    }
+                    R.id.add -> {
+                        itemSelected = HomeFragment()
+                    }
+                    R.id.chat -> {
+                        itemSelected = ChatFragment()
+                    }
+                    R.id.profile -> {
+                        itemSelected = ProfileFragment()
+                    }
                 }
-                R.id.search -> {
-                    itemSelected = SearchFragment()
-                }
-                R.id.add -> {
-                    itemSelected = HomeFragment()
-                }
-                R.id.chat -> {
-                    itemSelected = ChatFragment()
-                }
-                R.id.profile -> {
-                    itemSelected = ProfileFragment()
-                }
-            }
-            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerViewMainActivity, itemSelected).commit()
+                supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerViewMainActivity, itemSelected).commit()
             true
+        }
+        buttonAddAnnuncio.setOnClickListener {
+            val i = Intent(this,NuovoAnnuncioActivity::class.java)
+            startActivity(i)
         }
     }
 }
---
+++ app/src/main/java/com/example/usedbooks/dataClass/Studente.kt
@@ -0,0 +1,3 @@
+package com.example.usedbooks.dataClass
+
+data class Studente(val id : Int, val nome : String, val cognome : String, val username : String, val password : String)
