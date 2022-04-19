7de1e0ac74c6d01d774e675046411a1e8cbbdb09
Javaninja-Lab <luigi.kart30@gmail.com>
2022-04-19 10:00:28 +0200

    Inizio implementazione fragment nella MainActivity.kt

--- app/src/main/java/com/example/usedbooks/MainActivity.kt
+++ app/src/main/java/com/example/usedbooks/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.usedbooks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +11,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction().add(R.id.fragmentContainerView, HomeFragment()).commit()
-
+        supportFragmentManager.beginTransaction().add(R.id.fragmentContainerViewMainActivity, HomeFragment()).commit()
+        bottomAppBar.setOnMenuItemClickListener {
+            var itemSelected : Fragment = HomeFragment()
+            when(it.itemId) {
+                R.id.home -> {
+                    itemSelected = HomeFragment()
+                }
+                R.id.search -> {
+                    itemSelected = SearchFragment()
+                }
+                R.id.add -> {
+                    itemSelected = HomeFragment()
+                }
+                R.id.chat -> {
+                    itemSelected = ChatFragment()
+                }
+                R.id.profile -> {
+                    itemSelected = ProfileFragment()
+                }
+            }
+            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerViewMainActivity, itemSelected).commit()
+            true
+        }
     }
 }
