3904e0413530e44499d917c53cc8a3a7661f918b
Javaninja-Lab <luigi.kart30@gmail.com>
2022-04-20 11:03:24 +0200

    Inizio implementazione supporto transazioni fragment

--- app/src/main/java/com/example/usedbooks/ChatFragment.kt
+++ app/src/main/java/com/example/usedbooks/ChatFragment.kt
@@ -10,64 +10,11 @@ import com.example.usedbooks.dataClass.Database
 import kotlinx.android.synthetic.main.fragment_chat.*
 import org.postgresql.util.PSQLException
 import java.sql.DriverManager
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ChatFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ChatFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    // TODO: Implementare codice Fragment Chat
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-        try {
-            val connection =
-                DriverManager.getConnection(Database.jdbcUrl, "sql4486484", "Lw6ul89qq8")
-            val query = connection.prepareStatement("SELECT * FROM studenti")
-            val result = query.executeQuery()
-            testTextView.setText(result.getString("nome"))
-        }catch (e : PSQLException){
-            e.printStackTrace()
-        }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ChatFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ChatFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
--- app/src/main/java/com/example/usedbooks/MainActivity.kt
+++ app/src/main/java/com/example/usedbooks/MainActivity.kt
@@ -5,42 +5,77 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toolbar
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 
 class MainActivity : AppCompatActivity() {
+    private val homeFragment = HomeFragment()
+    private val searchFragment = SearchFragment()
+    private val chatFragment = ChatFragment()
+    private val profileFragment = ProfileFragment()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction().add(R.id.fragmentContainerViewMainActivity, HomeFragment()).commit()
+        supportFragmentManager.beginTransaction().add(R.id.fragmentContainerViewMainActivity, homeFragment).addToBackStack(homeFragment.tag).commit()
 
         bottomNavigationView.setOnItemSelectedListener {
-                var itemSelected: Fragment = HomeFragment()
-                when (it.itemId) {
-                    R.id.home -> {
-                        itemSelected = HomeFragment()
-                    }
-                    R.id.search -> {
-                        itemSelected = SearchFragment()
-                    }
-                    R.id.add -> {
-                        itemSelected = HomeFragment()
-                    }
-                    R.id.chat -> {
-                        itemSelected = ChatFragment()
-                    }
-                    R.id.profile -> {
-                        itemSelected = ProfileFragment()
-                    }
+            var itemSelected: Fragment = homeFragment
+            when (it.itemId) {
+                R.id.home -> {
+                    itemSelected = homeFragment
                 }
-                supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerViewMainActivity, itemSelected).commit()
+                R.id.search -> {
+                    itemSelected = searchFragment
+                }
+                R.id.chat -> {
+                    itemSelected = chatFragment
+                }
+                R.id.profile -> {
+                    itemSelected = profileFragment
+                }
+            }
+            if(supportFragmentManager.fragments.last().id != itemSelected.id){
+                if(supportFragmentManager.fragments.contains(itemSelected)){
+                    val lastFragment = supportFragmentManager.fragments.last()
+                    for(i in 0..supportFragmentManager.backStackEntryCount){
+                        supportFragmentManager.popBackStack()
+                    }
+                    supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerViewMainActivity, lastFragment).addToBackStack(null).commit()
+                }
+                supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerViewMainActivity, itemSelected).addToBackStack(null).commit()
+            }
             true
         }
         buttonAddAnnuncio.setOnClickListener {
             val i = Intent(this,NuovoAnnuncioActivity::class.java)
             startActivity(i)
         }
+    }
+
+    override fun onBackPressed() {
+        /*if(supportFragmentManager.backStackEntryCount > 0) {
+            when(supportFragmentManager.fragments.last().tag){
+                "home" -> {
+                    bottomNavigationView.selectedItemId = R.id.home
+                }
+                "search" -> {
+                    bottomNavigationView.selectedItemId = R.id.search
+                }
+                "chat" -> {
+                    bottomNavigationView.selectedItemId = R.id.chat
+                }
+                "profile" -> {
+                    bottomNavigationView.selectedItemId = R.id.profile
+                }
+            }
+        }*/
+        super.onBackPressed()
     }
 }
