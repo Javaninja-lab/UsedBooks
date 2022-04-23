da8c7caf99eb29fe87bca8345c4f446cd0019276
Zeref-zt <lorenzo.ficazzola@outlook.it>
2022-04-23 09:29:09 +0200

    inizio lavorazione firebase

--- app/src/main/java/com/example/usedbooks/RegisterActivity.kt
+++ app/src/main/java/com/example/usedbooks/RegisterActivity.kt
@@ -2,11 +2,17 @@ package com.example.usedbooks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.firestore.FirebaseFirestore
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        val db= FirebaseFirestore.getInstance();
+        val btn= findViewById<Button>(R.id.btn_Register)
+        btn.setOnClickListener {
+        }
     }
 
     override fun finish() {
