cedccfd3162afb4e8db0b7be5fd8f156f3644283
Zeref-zt <lorenzo.ficazzola@outlook.it>
2022-04-23 11:28:36 +0200

    firestore fa schifo

--- app/src/main/java/com/example/usedbooks/RegisterActivity.kt
+++ app/src/main/java/com/example/usedbooks/RegisterActivity.kt
@@ -1,17 +1,45 @@
 package com.example.usedbooks
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-        val db= FirebaseFirestore.getInstance();
+        val db= Firebase.firestore
         val btn= findViewById<Button>(R.id.btn_Register)
         btn.setOnClickListener {
+            val name= findViewById<EditText>(R.id.et_Name)
+            val surname= findViewById<EditText>(R.id.et_Surname)
+            val uni= findViewById<EditText>(R.id.et_University)
+            val email= findViewById<EditText>(R.id.et_email)
+            val password= findViewById<EditText>(R.id.et_Password)
+            val password2= findViewById<EditText>(R.id.et_ConfirmPassword)
+
+            Log.d("test","test")
+            val studente = hashMapOf(
+                "cognome" to surname.text.toString(),
+                "nome" to name.text.toString(),
+                "email" to email.text.toString(),
+                "password" to password.text.toString()
+            )
+            db.collection("studenti")
+                .add(studente)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "Error adding document", e)
+                }
+
         }
     }
 
