1405650dc88b43f67a40e562ac00a3eaeef326c2
Javaninja-Lab <luigi.kart30@gmail.com>
2022-04-26 18:06:37 +0200

    - Sostituzione ListView con RecyclerView

--- app/src/main/java/com/example/usedbooks/main/home/HomeFragment.kt
+++ app/src/main/java/com/example/usedbooks/main/home/HomeFragment.kt
@@ -7,8 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.os.bundleOf
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.example.usedbooks.R
 import com.example.usedbooks.dataClass.Database
 import com.example.usedbooks.dataClass.Materiale
@@ -22,54 +24,55 @@ class HomeFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-        val adapter = MaterialeAdapter(this.requireContext(), Database.getMateriali())
-        val lv_home = view.findViewById<ListView>(R.id.lv_home)
-        lv_home.adapter = adapter
-        lv_home.setOnItemClickListener { adapterView, view, position, id ->
-            val action = HomeFragmentDirections.actionHomeFragmentToMaterialeFragment(adapterView.getItemAtPosition(position) as Materiale)
-            view.findNavController().navigate(action)
-        }
+        val recyclerView  = view.findViewById<RecyclerView>(R.id.lv_home)
+        val adapter = MaterialeRecyclerAdapter()
+        recyclerView.adapter = adapter
+        adapter.submitList(Database.getMateriali())
+
         return view
     }
 }
 
-class MaterialeAdapter(private val context: Context,
-       private val dataSource: ArrayList<Materiale>) : BaseAdapter() {
-
-    private val inflater: LayoutInflater
-            = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-
-    override fun getCount(): Int {
-        return dataSource.size
+class MaterialeRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private lateinit var items : ArrayList<Materiale>
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = MaterialeViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item_home, parent, false))
+        return view
     }
 
-    override fun getItem(position: Int): Materiale? {
-        return dataSource[position]
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder) {
+            is MaterialeViewHolder -> {
+                holder.bind(items.get(position))
+                holder.itemView.setOnClickListener {
+                    val action = HomeFragmentDirections.actionHomeFragmentToMaterialeFragment(holder.materiale)
+                    it.findNavController().navigate(action)
+                }
+            }
+        }
     }
 
-    override fun getItemId(position: Int): Long {
-        return position.toLong()
+    override fun getItemCount(): Int {
+        return items.size
     }
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        // Get view for row item
-        var rowView : View
-        if(convertView == null)
-            rowView = inflater.inflate(R.layout.list_item_home, parent, false)
-        else
-            rowView = convertView
-
-        val materiale = getItem(position)
-
-        val iv_foto_materiale = rowView.findViewById<ImageView>(R.id.iv_foto_materiale)
-        //TODO(Mettere immagine presa dalla classe)
-        val tv_nome_materiale = rowView.findViewById<TextView>(R.id.tv_nome_materiale)
-        tv_nome_materiale.setText(materiale?.nome)
-        val tv_nome_venditore = rowView.findViewById<TextView>(R.id.tv_nome_venditore)
-        tv_nome_venditore.setText(materiale?.proprietario)
-        val tv_prezzo = rowView.findViewById<TextView>(R.id.tv_prezzo)
-        tv_prezzo.setText(materiale?.prezzo.toString())
-        return rowView
+    fun submitList(list : ArrayList<Materiale>) {
+        items = list
     }
 
+    class MaterialeViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        lateinit var materiale : Materiale
+
+        fun bind(materiale: Materiale) {
+            this.materiale = materiale
+            val iv_foto_materiale = itemView.findViewById<ImageView>(R.id.iv_foto_materiale)
+            //TODO(Mettere immagine presa dalla classe)
+            val tv_nome_materiale = itemView.findViewById<TextView>(R.id.tv_nome_materiale)
+            tv_nome_materiale.setText(materiale.nome)
+            val tv_nome_venditore = itemView.findViewById<TextView>(R.id.tv_nome_venditore)
+            tv_nome_venditore.setText(materiale.proprietario)
+            val tv_prezzo = itemView.findViewById<TextView>(R.id.tv_prezzo)
+            tv_prezzo.setText(materiale.prezzo.toString())
+        }
+    }
 }
