4d1a7aba74b907f7818d1515e4d6c8193dc0ada0
Javaninja-Lab <luigi.kart30@gmail.com>
2022-06-11 18:46:38 +0200

    - Aggiunta barra di caricamento quando si esegue una ricerca o si ottengono i materiali per la prima volta

--- app/src/main/java/com/example/usedbooks/main/home/HomeFragment.kt
+++ app/src/main/java/com/example/usedbooks/main/home/HomeFragment.kt
@@ -1,19 +1,23 @@
 package com.example.usedbooks.main.home
 
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import androidx.navigation.findNavController
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.usedbooks.R
 import com.example.usedbooks.adapters.MaterialeRecyclerAdapter
 import com.example.usedbooks.dataClass.Database
 import com.example.usedbooks.dataClass.Materiale
-import java.util.concurrent.Executors
 import kotlin.concurrent.thread
+
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
@@ -27,6 +31,17 @@ class HomeFragment : Fragment() {
         val adapter = MaterialeRecyclerAdapter("home")
         recyclerView.adapter = adapter
 
+        val pb_caricamento = view.findViewById<ProgressBar>(R.id.pb_caricamento)
+        val progressDrawable: Drawable = pb_caricamento.getIndeterminateDrawable().mutate()
+        val typedValue = TypedValue()
+        context?.getTheme()?.resolveAttribute(android.R.attr.colorPrimary, typedValue, true)
+        val color = typedValue.data
+        progressDrawable.colorFilter = BlendModeColorFilter(color, BlendMode.SRC_ATOP)
+        pb_caricamento.progressDrawable = progressDrawable
+
+
+
+
         val response: ArrayList<Materiale> = ArrayList()
         adapter.submitList(response)
 
@@ -36,6 +51,8 @@ class HomeFragment : Fragment() {
                 response.add(materiale)
             }
             this.activity?.runOnUiThread{
+                recyclerView.visibility = View.VISIBLE
+                pb_caricamento.visibility = View.GONE
                 adapter.notifyDataSetChanged()
             }
 
--- app/src/main/java/com/example/usedbooks/main/search/SearchFragment.kt
+++ app/src/main/java/com/example/usedbooks/main/search/SearchFragment.kt
@@ -1,12 +1,17 @@
 package com.example.usedbooks.main.search
 
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.TypedValue
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.usedbooks.R
@@ -35,23 +40,33 @@ class SearchFragment : Fragment() {
         val response: ArrayList<Materiale> = ArrayList()
         adapter.submitList(response)
 
+        val pb_caricamento = layout.findViewById<ProgressBar>(R.id.pb_caricamento)
+        val progressDrawable: Drawable = pb_caricamento.getIndeterminateDrawable().mutate()
+        val typedValue = TypedValue()
+        context?.getTheme()?.resolveAttribute(android.R.attr.colorPrimary, typedValue, true)
+        val color = typedValue.data
+        progressDrawable.colorFilter = BlendModeColorFilter(color, BlendMode.SRC_ATOP)
+        pb_caricamento.progressDrawable = progressDrawable
+
         thread(start = true) {
             response.clear()
             for (materiale in Database.getMateriali()){
                 response.add(materiale)
             }
             this.activity?.runOnUiThread{
+                recyclerView.visibility = View.VISIBLE
+                pb_caricamento.visibility = View.GONE
                 adapter.notifyDataSetChanged()
             }
-
         }
 
         val bt_search = layout.findViewById<Button>(R.id.bt_search)
         bt_search?.setOnClickListener {
 
+            recyclerView.visibility = View.GONE
+            pb_caricamento.visibility = View.VISIBLE
             val search = layout.findViewById<EditText>(R.id.et_cerca_annunci)?.text.toString()
             response.clear()
-            adapter.notifyDataSetChanged()
 
             thread(start = true) {
                 for (materiale in Database.searchMateriale(search)){
@@ -59,6 +74,8 @@ class SearchFragment : Fragment() {
                         response.add(materiale)
                 }
                 this.activity?.runOnUiThread{
+                    recyclerView.visibility = View.VISIBLE
+                    pb_caricamento.visibility = View.GONE
                     if(response.size==0)
                         Toast.makeText(context, "Nessun risultato", Toast.LENGTH_LONG).show()
                     else
