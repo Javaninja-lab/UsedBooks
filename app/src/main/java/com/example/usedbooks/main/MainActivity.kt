c783d5e7e33e25f08bca13fa1fe5871f1dd26a62
Javaninja-Lab <luigi.kart30@gmail.com>
2022-06-13 10:53:11 +0200

    Risolto problema fragment visualizzato sotto BottomAppBar

--- app/src/main/java/com/example/usedbooks/main/MainActivity.kt
+++ app/src/main/java/com/example/usedbooks/main/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.usedbooks.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainer
+import androidx.fragment.app.FragmentContainerView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.usedbooks.R
@@ -16,6 +20,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fg_main = findViewById<FragmentContainerView>(R.id.fg_main)
 
         val navController = findNavController(R.id.fg_main)
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
