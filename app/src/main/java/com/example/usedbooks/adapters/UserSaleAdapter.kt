77ca9b1cb5ad5dda0c4bb4e740d8a8df2bb1c27e
Zeref-zt <lorenzo.ficazzola@outlook.it>
2022-06-14 12:54:58 +0200

    da fare user sale adapter

--- app/src/main/java/com/example/usedbooks/adapters/UserSaleAdapter.kt
+++ app/src/main/java/com/example/usedbooks/adapters/UserSaleAdapter.kt
@@ -14,39 +14,7 @@ import com.example.usedbooks.dataClass.User
 import com.example.usedbooks.firebase.UserAdapter
 import com.example.usedbooks.main.chat.ListChatsFragmentDirections
 
-class UserSaleAdapter (val context: Context?, val userList: ArrayList<User>) :
-    RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
-
-    class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val mittente = itemView.findViewById<TextView>(R.id.tv_nome_studente)
-        val fotoProfilo = itemView.findViewById<ImageView>(R.id.iv_foto_profilo)
-        val utlimoMessaggio = itemView.findViewById<TextView>(R.id.tv_ultimo_messaggio)
-        val dataMessaggio = itemView.findViewById<TextView>(R.id.tv_data_messaggio)
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
-        val view: View = LayoutInflater.from(context).inflate(R.layout.list_item_chat,parent,false)// da vedere se funziona
-        return UserViewHolder(view)
-    }
+class UserSaleAdapter () {
 
 
-    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
-        val currentUser = userList[position]
-        holder.mittente.text = currentUser.username
-
-        //holder.fotoProfilo.setImageBitmap()
-        //TODO("Asseganre immagine alla chat")
-        val messaggio = Messaggio("ciao","1")  //Database.getLastMessage(currentUser)
-        //TODO("Aggiungere data al messaggio")
-        holder.dataMessaggio.text = messaggio.message
-        holder.utlimoMessaggio.text = messaggio.message  //messaggio.message?.subSequence(0..32)
-        holder.itemView.setOnClickListener {
-            val action = ListChatsFragmentDirections.actionChatFragmentToTextChat(currentUser.username!!, currentUser.id!!)
-            holder.itemView.findNavController().navigate(action)
-        }
-    }
-
-    override fun getItemCount(): Int {
-        return userList.size
-    }
 }
